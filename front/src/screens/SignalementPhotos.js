378c13b455e34bdfdfb32af2ee781b688a30cbc4
Mathis Cadio <cadio@et.esiea.fr>
2022-12-13 13:54:44 +0100

    Add camera and permission request

--- front/src/screens/SignalementPhotos.js
+++ front/src/screens/SignalementPhotos.js
@@ -1,14 +1,63 @@
 import * as React from 'react';
-import { HStack, VStack, Text, Box, Pressable, Center, Spacer } from "native-base";
+import { HStack, VStack, Text, Box, Pressable, Spacer, Button } from "native-base";
 import { Camera, CameraType } from 'expo-camera';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import StepButton from '../components/StepButton';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
 class SignalementPhotos extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      permission: true,
+      type: CameraType.back
+    }
+  }
 
+  componentDidMount() {
+    this._requestPermission()
+  }
+
+  async _requestPermission() {
+    let permission = await Camera.requestCameraPermissionsAsync()
+    this.setState({permission: permission});
+  }
+
+  _toggleCameraType() {
+    this.setState({
+      type: this.state.type === CameraType.back ? CameraType.front : CameraType.back
+    })
+  }
+
+  _testpermission() {
+    if (!this.state.permission) {
+      // Camera permissions are still loading
+      return <Box />;
+    }
+
+    if (!this.state.permission.granted) {
+      return (
+        <Box alignItems="center">
+          <Text>Vous devez autoriser l'utilisation de la camera</Text>
+          <Button colorScheme="warning" onPress={() => this._requestPermission()}>
+            Donner la permission
+          </Button>
+        </Box>
+      );
+    }
+  }
+
+  _camera() {
+    if (this.state.permission.granted) {
+      return (
+        <Camera style={styles.camera} type={this.state.type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => this._toggleCameraType()}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )
     }
   }
 
@@ -38,6 +87,12 @@ class SignalementPhotos extends React.Component {
             </Pressable>
           </Box>
         </HStack>
+        <HStack flex={1}>
+          <VStack flex={1}>
+            {this._testpermission()}
+            {this._camera()}
+          </VStack>
+        </HStack>
         <Text fontSize="2xl" fontWeight="bold">Vos photos</Text>
         <Text>Aucune photos</Text>
         <StepButton></StepButton>
@@ -45,5 +100,31 @@ class SignalementPhotos extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export default SignalementPhotos
