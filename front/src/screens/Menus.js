480141953c7217f4a93d7dc8a08c15fe6fa2c1f7
Mathis Cadio <cadio@et.esiea.fr>
2022-12-14 13:11:41 +0100

    final fix

--- front/src/screens/Menus.js
+++ front/src/screens/Menus.js
@@ -7,6 +7,7 @@ import MenusData from '../assets/MenusData.json';
 import MenusComponent from '../components/MenusComponent';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from "moment/moment";
 
 class Menus extends React.Component {
   constructor(props) {
@@ -25,9 +26,8 @@ class Menus extends React.Component {
   }
 
   _loadMenu() {
-    console.log(this.state.date.toLocaleDateString("fr-FR"))
     let menu = MenusData.find(obj => {
-      if (obj.date == this.state.date.toLocaleDateString("fr-FR"))
+      if (obj.date == moment(this.state.date).format('D/MM/YYYY'))
         return obj;
     })
     this.setState({current_menu: menu});
@@ -35,59 +35,67 @@ class Menus extends React.Component {
 
   _onChange(selectedDate) {
     this.setState({
-      showDatePicker: !this.state.showDatePicker,
-      date: new Date(selectedDate)
+      date: new Date(selectedDate),
+      showDatePicker: false
     })
     this._loadMenu();
   };
 
   render() {
     return (
-      <View style={styles.container}>
+      <Box style={styles.container}>
         <Box mx="4" my="6">
-            <Flex direction="row">
-              <Pressable onPress={() => this.props.navigation.navigate("Cantines")}><Ionicons name="chevron-back" size={30} color="black"/></Pressable>
-              <Text style={styles.title} pt="2">Convives</Text>
-            </Flex>
-            <Box mt="2" style={styles.selectContainer}>
-              <Select value={this.state.convives} defaultValue="adulte" style={styles.select} minWidth="200" accessibilityLabel="Convives" placeholder="Convives" mt="1" onValueChange={itemValue => {this.setState({convives: itemValue})}}>
-                <Select.Item label="Adulte" value="adulte" />
-                <Select.Item label="Maternelle" value="maternelle" />
-                <Select.Item label="Primaire" value="primaire" />
-              </Select>
-             </Box>
-             <Box my="3" style={styles.btnContainer}>
-                 <View>
-                   <Button onPress={() => this.setState({type_menu: "dejeuner"})} style={this.state.type_menu == "dejeuner" ? styles.btnStyle(1) : styles.btnStyle(0)}>
-                        <Text style={this.state.type_menu == "dejeuner" ? styles.btnText(1) : styles.btnText(0)}>Déjeuner</Text>
-                   </Button>
-                 </View>
-                 <View >
-                   <Button onPress={() => this.setState({type_menu: "SV"})} style={this.state.type_menu == "SV" ? styles.btnStyle(1) : styles.btnStyle(0)} >
-                       <Text style={this.state.type_menu == "SV" ? styles.btnText(1) : styles.btnText(0)}>Sans viande</Text>
-                   </Button>
-                 </View>
-                 <View>
-                   <Button onPress={() => this.setState({type_menu: "SP"})} style={this.state.type_menu == "SP" ? styles.btnStyle(1) : styles.btnStyle(0)}>
-                        <Text style={this.state.type_menu == "SP" ? styles.btnText(1) : styles.btnText(0)}>Sans porc</Text>
-                    </Button>
-                 </View>
-             </Box>
-             <Box style={styles.dateContainer}>
-              {this.state.showDatePicker &&
-                <DateTimePicker
-                  value={this.state.date}
-                  mode="date"
-                  minimumDate={new Date(2021, 12, 13)}
-                  style={styles.datePicker}
-                  onChange={(event, date) => {this._onChange(date);}}
-                />
-              }
-              <Pressable onPress={() => this._onChange(new Date(this.state.date.setDate(this.state.date.getDate() + 1)))}><Entypo name="chevron-right" size={30} color="black"/></Pressable>
-             </Box>
-             <MenusComponent convives={this.state.convives} type_menu={this.state.type_menu} menu={this.state.current_menu}/>
+          <Flex direction="row">
+            <Pressable onPress={() => this.props.navigation.navigate("Cantines")}><Ionicons name="chevron-back" size={30} color="black"/></Pressable>
+            <Text style={styles.title} pt="2">Convives</Text>
+          </Flex>
+          <Box mt="2" style={styles.selectContainer}>
+            <Select value={this.state.convives} defaultValue="adulte" style={styles.select} minWidth="200" accessibilityLabel="Convives" placeholder="Convives" mt="1" onValueChange={itemValue => {this.setState({convives: itemValue})}}>
+              <Select.Item label="Adulte" value="adulte" />
+              <Select.Item label="Maternelle" value="maternelle" />
+              <Select.Item label="Primaire" value="primaire" />
+            </Select>
+          </Box>
+          <Box my="3" style={styles.btnContainer}>
+                <View>
+                  <Button onPress={() => this.setState({type_menu: "dejeuner"})} style={this.state.type_menu == "dejeuner" ? styles.btnStyle(1) : styles.btnStyle(0)}>
+                      <Text style={this.state.type_menu == "dejeuner" ? styles.btnText(1) : styles.btnText(0)}>Déjeuner</Text>
+                  </Button>
+                </View>
+                <View >
+                  <Button onPress={() => this.setState({type_menu: "SV"})} style={this.state.type_menu == "SV" ? styles.btnStyle(1) : styles.btnStyle(0)} >
+                      <Text style={this.state.type_menu == "SV" ? styles.btnText(1) : styles.btnText(0)}>Sans viande</Text>
+                  </Button>
+                </View>
+                <View>
+                  <Button onPress={() => this.setState({type_menu: "SP"})} style={this.state.type_menu == "SP" ? styles.btnStyle(1) : styles.btnStyle(0)}>
+                      <Text style={this.state.type_menu == "SP" ? styles.btnText(1) : styles.btnText(0)}>Sans porc</Text>
+                  </Button>
+                </View>
+          </Box>
+          <Box style={styles.dateContainer} bg="#C30065">
+            <Pressable onPress={() => this._onChange(new Date(this.state.date.setDate(this.state.date.getDate() - 1)))}>
+              <Entypo name="chevron-left" size={30} color="white"/>
+            </Pressable>
+            <Pressable onPress={() => this.setState({showDatePicker: true})}>
+              <Text color="white" fontSize="lg">{moment(this.state.date).format('D/MM/YYYY')}</Text>
+            </Pressable>
+            {this.state.showDatePicker &&
+              <DateTimePicker
+                value={this.state.date}
+                mode="date"
+                minimumDate={new Date(2021, 12, 13)}
+                style={styles.datePicker}
+                onChange={(event, date) => {this._onChange(date);}}
+              />
+            }
+            <Pressable onPress={() => this._onChange(new Date(this.state.date.setDate(this.state.date.getDate() + 1)))}>
+              <Entypo name="chevron-right" size={30} color="white"/>
+            </Pressable>
+          </Box>
+          <MenusComponent convives={this.state.convives} type_menu={this.state.type_menu} menu={this.state.current_menu}/>
         </Box>
-      </View>
+      </Box>
     )
   }
 }
@@ -169,11 +177,11 @@ const styles = StyleSheet.create({
   dateContainer:{
     flexDirection:"row",
     justifyContent: "center",
-    alignItems:"center",
+    alignItems: "center",
+    alignContent: "center",
     flexWrap:"wrap",
     height: 40,
-    borderRadius: 5,
-    backgroundColor: "#C30065",
+    borderRadius: 5
   }
 })
 
