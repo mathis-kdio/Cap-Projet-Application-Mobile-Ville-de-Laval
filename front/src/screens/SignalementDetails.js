978994c4697673c2915b62ed6d728574ad0502da
Mathis Cadio <cadio@et.esiea.fr>
2022-12-14 11:16:39 +0100

    Get text in SignalementDetails

--- front/src/screens/SignalementDetails.js
+++ front/src/screens/SignalementDetails.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { HStack, VStack, Text, TextArea, Box, Spacer } from "native-base";
-import MozaicButton from '../components/MozaicButton';
+import { VStack, Text, TextArea, Spacer } from "native-base";
 import StepButton from '../components/StepButton';
 
 class SignalementDetails extends React.Component {
   constructor(props) {
     super(props)
+    this.details = ""
     this.state = {
 
     }
@@ -19,11 +19,21 @@ class SignalementDetails extends React.Component {
       }
     })
   }
+
+  _textAreaChanged(text) {
+    this.details = text;
+  }
+
   render() {
     return (
       <VStack flex={1} marginX={5}>
         <Text fontSize="2xl" fontWeight="bold">Quel est le problème ?</Text>
-        <TextArea h={20} placeholder="Décrivez le problème ici" w="100%"/>
+        <TextArea 
+          h={20}
+          placeholder="Décrivez le problème ici"
+          w="100%"
+          onChangeText={(text) => this._textAreaChanged(text)}
+        />
         <Spacer/>
         <StepButton _navigation={() => this._navigation()}/>
       </VStack>
